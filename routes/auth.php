638f8542ffd4d0900d5c3e80724b98520be500d3
Samuel AGOUA <sagoua1@gmail.com>
2023-10-30 01:09:42 +0000

    register process fix

--- routes/auth.php
+++ routes/auth.php
@@ -33,11 +33,13 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
+    Route::get('verify-email', EmailVerificationPromptController::class)
+        ->name('verification.notice');
 });
 
+
+
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-                ->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
                 ->middleware(['signed', 'throttle:6,1'])
