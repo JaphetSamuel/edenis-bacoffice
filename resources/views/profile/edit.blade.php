89c546d80ae30dad6bb9e01098d080f4fe6ba3e1
Samuel AGOUA <sagoua1@gmail.com>
2023-11-11 10:23:56 +0000

    resize

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -10,13 +10,13 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="p-4 sm:p-8 shadow sm:rounded-lg">
                     <div class="max-w-xl">
                         @include('profile.partials.show-profile-information')
                     </div>
                 </div>
 
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="p-4 sm:p-8 shadow sm:rounded-lg">
                     <div class="max-w-xl">
                         @include('profile.partials.update-password-form')
                     </div>
--- resources/views/profile/partials/show-profile-information.blade.php
+++ resources/views/profile/partials/show-profile-information.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Profile Information') }}
+        <h2 class="text-lg font-medium text-primary text-bold">
+            {{ __('Profil Informations') }}
         </h2>
     </header>
 
@@ -18,7 +18,7 @@
         <span class="bg-gray-100 rounded mr-2 px-1 py-1">
             {{ route('register', ['code' => $user->parrain_code]) }}
         </span>
-        <button class="bg-gray-800 dark:bg-gray-200 rounded-md px-4 py-1 text-white dark:text-gray-100"
+        <button class=" bg-primary rounded px-4 py-1 text-gray-dark dark:text-gray-100"
                 onClick="navigator.clipboard.writeText(`{{ route('register', ['code' => $user->parrain_code]) }}`);;alert('copié')">
             {{ __('Copy') }}
         </button>
