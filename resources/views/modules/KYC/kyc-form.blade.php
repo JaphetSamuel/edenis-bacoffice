04e4e47df3579e8e2bef64f72ee5c9b78c8b4db7
Samuel AGOUA <sagoua1@gmail.com>
2023-11-12 20:23:17 +0000

    style maj

--- resources/views/modules/KYC/kyc-form.blade.php
+++ resources/views/modules/KYC/kyc-form.blade.php
@@ -3,7 +3,7 @@
         input {
             border-color: #1a202c;
             border-width: 1px;
-            color: #0a0e14;
+            color: #0a0e14 !important;
         }
     </style>
     <form wire:submit="create" enctype="multipart/form-data">
