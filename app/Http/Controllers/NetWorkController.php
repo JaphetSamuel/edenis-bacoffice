3d3bf6af09483791e2f175ed20f41da59f9a2069
Samuel AGOUA <sagoua1@gmail.com>
2023-12-03 00:19:53 +0000

    withdrawal

--- app/Http/Controllers/NetWorkController.php
+++ app/Http/Controllers/NetWorkController.php
@@ -11,6 +11,7 @@ class NetWorkController extends Controller
         $user = auth()->user();
 
         $filleuls = $user->filleuls()->get();
+        dd($filleuls);
 
 
         return view('modules.network.index', [
