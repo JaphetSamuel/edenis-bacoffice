857ba4cc86ed97acdc17cf92a074cbf3c53f4756
Samuel AGOUA <sagoua1@gmail.com>
2023-12-10 15:30:06 +0000

    withdraw resource

--- app/Filament/Resources/WithdrawalResource.php
+++ app/Filament/Resources/WithdrawalResource.php
@@ -46,8 +46,8 @@ class WithdrawalResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('portefeuille.user')
-                    ->state(fn($state)=> $state->name.' '.$state->lastname)
+                Tables\Columns\TextColumn::make('portefeuille.user.name')
+//                    ->state(fn($state)=> $state->name.' '.$state->lastname)
                     ->label('Name')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('amount')
